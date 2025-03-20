07fac4e34432524eddc874987ce15408402fdad4
Francisco Octavio Ayra Caceres <franciscoayracaceress@gmail.com>
2025-03-20 16:46:11 -0400

    prueba 1 de carga de mapa en formato MBTiles (fallida)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'map_screen.dart';
+import 'package:flutter_map_mbtiles/flutter_map_mbtiles.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,51 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(Object context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late MbTilesTileProvider _mbTilesProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    _mbTilesProvider = MbTilesTileProvider.fromPath(
+      path: 'assets/tiles/tiles.mbtiles',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Mapa Offline con MBTiles")),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(
+            37.7749,
+            -122.4194,
+          ), // Coordenadas de ejemplo (San Francisco)
+          initialZoom: 5,
+        ),
+        children: [
+          TileLayer(
+            tileProvider: _mbTilesProvider,
+            urlTemplate: "", // Se deja vacío ya que usa MBTiles
+          ),
+        ],
+      ),
+    );
   }
 }
