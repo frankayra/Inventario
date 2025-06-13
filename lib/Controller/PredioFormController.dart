d1b0ba7d0389f7c3e45170f71f53059d373cd88e
Francisco Octavio Ayra Caceres <franciscoayracaceress@gmail.com>
2025-06-12 21:44:59 -0400

    Arquitectura MVC [50%] Predio Controller

--- lib/Controller/PredioFormController.dart
+++ lib/Controller/PredioFormController.dart
@@ -1,1 +1,63 @@
-class PredioFormController {}
+import 'package:flutter/material.dart';
+import 'package:inventario/Model/db_general_management.dart' as db;
+import 'package:inventario/presentation/Widgets/dialogs.dart';
+import 'package:inventario/Model/wrappers.dart';
+
+class PredioFormController {
+  final _dropdownOptions = {
+    "acera": {0: 'No existe', 1: 'Bueno', 2: 'Regular', 3: 'Malo'},
+  };
+  bool changePredio = false;
+  int? idPredio;
+  double? nivelPredio1;
+  double? nivelPredio2;
+  double? nivelPredio3;
+  int? acera;
+  double? anchoAcera;
+  String? observacionesTerreno;
+
+  Future<void> validateForm(
+    GlobalKey<FormState> formKey,
+    BuildContext context,
+    FormGlobalStatusWrapper<int> formGlobalStatus,
+  ) async {
+    if (formKey.currentState!.validate()) {
+      if (idPredio == null ||
+          idPredio! < 1000000000 ||
+          idPredio! >= 10000000000) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('❌ Número de localización no válido')),
+        );
+        return;
+      }
+      var oldPredio = await db.getPredio(idPredio: idPredio!);
+      if (oldPredio != null) {
+        var override = await showAcceptDismissAlertDialog(
+          context,
+          message:
+              "Se sobrescribirá la información del predio actual. ¿Desea continuar?",
+        );
+        if (override == null || !override) return;
+      }
+      final predio = db.Predio(
+        idPredio: formGlobalStatus["idPredio"]!,
+        nivelPredio1: nivelPredio1!,
+        nivelPredio2: nivelPredio2!,
+        nivelPredio3: nivelPredio3!,
+        acera: acera!,
+        anchoAcera: anchoAcera!,
+        observacionesTerreno: observacionesTerreno,
+      );
+      try {
+        await predio.insertInDB();
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('✅ Datos guardados')));
+      } catch (e) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('❌ Error al guardar los datos')));
+      }
+    }
+  }
+}
--- lib/presentation/Screens/Form sections/Predio.dart
+++ lib/presentation/Screens/Form sections/Predio.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart';
 import 'package:inventario/Model/db_general_management.dart' as db;
 import 'package:inventario/Model/wrappers.dart';
 import 'package:inventario/presentation/Widgets/dialogs.dart';
+import 'package:inventario/Controller/PredioFormController.dart';
 
 class PredioForm extends StatefulWidget {
   final FormGlobalStatusWrapper<int> formGlobalStatus;
@@ -18,40 +19,26 @@ class PredioForm extends StatefulWidget {
 // DONE: Cuando ya existe el predio en la BD, este formulario debe autorellenar sus campos con los datos de la tupla presentes en la BD.
 class PredioFormState extends State<PredioForm> {
   final _formKey = GlobalKey<FormState>();
+  final controller = PredioFormController();
   final _dropdownOptions = {
     "acera": {0: 'No existe', 1: 'Bueno', 2: 'Regular', 3: 'Malo'},
   };
   bool changePredio = false;
-  int? idPredio;
-  double? _nivelPredio1;
-  double? _nivelPredio2;
-  double? _nivelPredio3;
-  int? _acera;
-  double? _anchoAcera;
-  String? _observacionesTerreno;
 
   @override
   void initState() {
     super.initState();
     if (widget.formGlobalStatus["idPredio"] != null) {
-      idPredio = widget.formGlobalStatus["idPredio"];
-      db.getPredio(idPredio: idPredio!).then((predio) {
+      controller.idPredio = widget.formGlobalStatus["idPredio"];
+      db.getPredio(idPredio: controller.idPredio!).then((predio) {
         if (predio != null) {
-          print("""
-            nivelPredio1: ${predio.nivelPredio1}
-            nivelPredio2: ${predio.nivelPredio2}
-            nivelPredio3: ${predio.nivelPredio3}
-            acera: ${predio.acera}
-            anchoAcera: ${predio.anchoAcera}
-            observacionesTerreno: ${predio.observacionesTerreno}
-          """);
           setState(() {
-            _nivelPredio1 = predio.nivelPredio1;
-            _nivelPredio2 = predio.nivelPredio2;
-            _nivelPredio3 = predio.nivelPredio3;
-            _acera = predio.acera;
-            _anchoAcera = predio.anchoAcera;
-            _observacionesTerreno = predio.observacionesTerreno;
+            controller.nivelPredio1 = predio.nivelPredio1;
+            controller.nivelPredio2 = predio.nivelPredio2;
+            controller.nivelPredio3 = predio.nivelPredio3;
+            controller.acera = predio.acera;
+            controller.anchoAcera = predio.anchoAcera;
+            controller.observacionesTerreno = predio.observacionesTerreno;
           });
         }
       });
@@ -68,9 +55,11 @@ class PredioFormState extends State<PredioForm> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             TextFormField(
-              key: ValueKey("nivelPredio1-$_nivelPredio1"),
+              key: ValueKey("nivelPredio1-${controller.nivelPredio1}"),
               initialValue:
-                  _nivelPredio1 != null ? _nivelPredio1.toString() : "",
+                  controller.nivelPredio1 != null
+                      ? controller.nivelPredio1.toString()
+                      : "",
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Nivel predio 1'),
               validator: (value) {
@@ -81,14 +70,16 @@ class PredioFormState extends State<PredioForm> {
                 if (number == null) {
                   return 'Por favor ingresa un número decimal válido';
                 }
-                _nivelPredio1 = number;
+                controller.nivelPredio1 = number;
                 return null;
               },
             ),
             TextFormField(
-              key: ValueKey("nivelPredio2-$_nivelPredio2"),
+              key: ValueKey("nivelPredio2-${controller.nivelPredio2}"),
               initialValue:
-                  _nivelPredio2 != null ? _nivelPredio2.toString() : "",
+                  controller.nivelPredio2 != null
+                      ? controller.nivelPredio2.toString()
+                      : "",
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Nivel predio 2'),
               validator: (value) {
@@ -99,14 +90,16 @@ class PredioFormState extends State<PredioForm> {
                 if (number == null) {
                   return 'Por favor ingresa un número decimal válido';
                 }
-                _nivelPredio2 = number;
+                controller.nivelPredio2 = number;
                 return null;
               },
             ),
             TextFormField(
-              key: ValueKey("nivelPredio3-$_nivelPredio3"),
+              key: ValueKey("nivelPredio3-${controller.nivelPredio3}"),
               initialValue:
-                  _nivelPredio3 != null ? _nivelPredio3.toString() : "",
+                  controller.nivelPredio3 != null
+                      ? controller.nivelPredio3.toString()
+                      : "",
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Nivel predio 3'),
               validator: (value) {
@@ -117,12 +110,12 @@ class PredioFormState extends State<PredioForm> {
                 if (number == null) {
                   return 'Por favor ingresa un número decimal válido';
                 }
-                _nivelPredio3 = number;
+                controller.nivelPredio3 = number;
                 return null;
               },
             ),
             DropdownButtonFormField(
-              value: _acera,
+              value: controller.acera,
               items: _dropdownOptions["acera"]!.entries
                   .map((tipoAcera) {
                     return DropdownMenuItem(
@@ -133,7 +126,7 @@ class PredioFormState extends State<PredioForm> {
                   .toList(growable: false),
               onChanged: (value) {
                 setState(() {
-                  _acera = value;
+                  controller.acera = value;
                 });
               },
               decoration: InputDecoration(labelText: 'Acera'),
@@ -145,8 +138,11 @@ class PredioFormState extends State<PredioForm> {
               },
             ),
             TextFormField(
-              key: ValueKey("anchoAcera-$_anchoAcera"),
-              initialValue: _anchoAcera != null ? _anchoAcera.toString() : "",
+              key: ValueKey("anchoAcera-${controller.anchoAcera}"),
+              initialValue:
+                  controller.anchoAcera != null
+                      ? controller.anchoAcera.toString()
+                      : "",
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Ancho de la acera'),
               validator: (value) {
@@ -157,66 +153,32 @@ class PredioFormState extends State<PredioForm> {
                 if (number == null) {
                   return 'Por favor ingresa un número válido';
                 }
-                _anchoAcera = number;
+                controller.anchoAcera = number;
                 return null;
               },
             ),
             TextFormField(
-              key: ValueKey("observacionesTerreno-$_observacionesTerreno"),
+              key: ValueKey(
+                "observacionesTerreno-${controller.observacionesTerreno}",
+              ),
               initialValue:
-                  _observacionesTerreno != null
-                      ? _observacionesTerreno.toString()
+                  controller.observacionesTerreno != null
+                      ? controller.observacionesTerreno.toString()
                       : "",
               decoration: InputDecoration(labelText: 'Observaciones terreno'),
               onChanged: (value) {
-                _observacionesTerreno = value;
+                controller.observacionesTerreno = value;
               },
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    if (idPredio == null ||
-                        idPredio! < 1000000000 ||
-                        idPredio! >= 10000000000) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('❌ Número de localización no válido'),
-                        ),
-                      );
-                      return;
-                    }
-                    var oldPredio = await db.getPredio(idPredio: idPredio!);
-                    if (oldPredio != null) {
-                      var override = await showAcceptDismissAlertDialog(
-                        context,
-                        message:
-                            "Se sobrescribirá la información del predio actual. ¿Desea continuar?",
-                      );
-                      if (override == null || !override) return;
-                    }
-                    final predio = db.Predio(
-                      idPredio: widget.formGlobalStatus["idPredio"]!,
-                      nivelPredio1: _nivelPredio1!,
-                      nivelPredio2: _nivelPredio2!,
-                      nivelPredio3: _nivelPredio3!,
-                      acera: _acera!,
-                      anchoAcera: _anchoAcera!,
-                      observacionesTerreno: _observacionesTerreno,
-                    );
-                    try {
-                      await predio.insertInDB();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('✅ Datos guardados')),
-                      );
-                    } catch (e) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('❌ Error al guardar los datos')),
-                      );
-                    }
-                  }
-                },
+                onPressed:
+                    () async => await controller.validateForm(
+                      _formKey,
+                      context,
+                      widget.formGlobalStatus,
+                    ),
                 child: Text('Guardar'),
               ),
             ),
